93b82b094ae3a20ef6c291f95aa63fdc4efd556a
TANVEER2935 <160842616+TANVEER2935@users.noreply.github.com>
2025-07-07 15:20:06 +0530

    Update App.jsx

--- client/soket/src/App.jsx
+++ client/soket/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 // Connect to backend server
-const socket = io("http://localhost:3000");
+const socket = io("chatapp-production-a600.up.railway.app");
 
 const App = () => {
   const [message, setMessage] = useState("");
